00a3738e3b20562a71b047a9d654e99f3118d693
hemadri-git <bathulahemadri@gmail.com>
2024-08-24 12:47:57 +0530

    push

--- arrays/2dMUL.c
+++ arrays/2dMUL.c
@@ -5,7 +5,7 @@ int main()
     printf("enter n*n matrices: ");
     scanf("%d", &n);
 
-    int a[n][n], b[n][n];
+    int a[n][n], b[n][n] , result[n][n];
 
     printf("input first matrices : \n");
     for (int i = 0; i < n; i++)
@@ -28,6 +28,30 @@ int main()
         }
         
     }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = 0;
+        }
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+            
+            
+        }
+        
+    }
+    
 
      printf("output first matrices : \n");
     for (int i = 0; i < n; i++)
@@ -59,7 +83,7 @@ int main()
         for (int j = 0; j < n; j++)
         {
             
-            printf("%d ", a[i][j] * b[i][j]);
+            printf("%d ", result[i][j]);
         }
         printf("\n");
         
